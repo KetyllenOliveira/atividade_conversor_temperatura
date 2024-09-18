fe08c2028d4085cd572df9a79b133be6dd5623ce
Ketyllen Emilly <96392358+KetyllenOliveira@users.noreply.github.com>
2024-09-18 13:36:49 -0300

    Add files via upload

---
+++ atividade_conversor_temperatura/src/main/java/poo/atividade_conversor_temperatura/AtividadeConversorTemperaturaApplication.java
@@ -0,0 +1,13 @@
+package poo.atividade_conversor_temperatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeConversorTemperaturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeConversorTemperaturaApplication.class, args);
+	}
+
+}
---
+++ atividade_conversor_temperatura/src/test/java/poo/atividade_conversor_temperatura/AtividadeConversorTemperaturaApplicationTests.java
@@ -0,0 +1,13 @@
+package poo.atividade_conversor_temperatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeConversorTemperaturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
